336deb170c8cac14130941649e7768d6bb9388f9
LinsThi <thiagolins13255@gmail.com>
2022-05-13 19:03:50 -0300

    :sparkles: Created LoginScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import { LoginScreen } from './modules/Login/Views/LoginScreen';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world</Text>
-    </View>
-  );
+  return <LoginScreen />;
 }
---
+++ src/modules/Login/Views/LoginScreen/styles.ts
@@ -0,0 +1,36 @@
+import { Dimensions, ImageBackground } from 'react-native';
+import styled from 'styled-components/native';
+
+import loginBackground from '~/shared/assets/loginBackground.png';
+
+export const Container = styled(ImageBackground).attrs({
+  source: loginBackground,
+})`
+  flex: 1;
+  align-items: center;
+`;
+
+export const ContainerImg = styled.View`
+  flex: 0.4;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const ContainerInputs = styled.View`
+  flex: 0.3;
+  margin-top: 40px;
+  width: 80%;
+  justify-content: center;
+`;
+
+export const ContainerButtons = styled.View`
+  flex: 0.2;
+  width: 226px;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Image = styled.Image`
+  width: 200px;
+  height: 180px;
+`;
