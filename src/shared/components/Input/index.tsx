8cb7f0b1f47d757b480c28ea75767567793347e8
LinsThi <thiagolins13255@gmail.com>
2022-05-13 19:01:02 -0300

    :sparkles: Created Input component

---
+++ src/shared/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import * as Sty from './styles';
+
+export type InputProps = {
+  label: string;
+  border?: boolean;
+  iconLeftName: string;
+  iconLeftType: string;
+  iconLeftAction?: () => void;
+} & TextInputProps;
+
+export function Input({
+  label,
+  border = false,
+  iconLeftName,
+  iconLeftType,
+  iconLeftAction,
+  ...rest
+}: InputProps) {
+  return (
+    <Sty.Container>
+      <Sty.LabelInput>{label}</Sty.LabelInput>
+
+      <Sty.ContainerInput border={border}>
+        <Sty.Input {...rest} />
+
+        <Sty.ButtonIcon onPress={iconLeftAction}>
+          <Sty.IconInput name={iconLeftName} type={iconLeftType} />
+        </Sty.ButtonIcon>
+      </Sty.ContainerInput>
+    </Sty.Container>
+  );
+}
