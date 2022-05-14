f9cd4c9e40c0912c29aa434dda27126ffd208103
LinsThi <thiagolins13255@gmail.com>
2022-05-14 20:34:50 -0300

    :sparkles: Adding new route

--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -7,10 +7,15 @@ import { HomeScreen } from '~/modules/Home/Views/HomeScreen';
 import { LoginScreen } from '~/modules/Login/Views/LoginScreen';
 import { RegisterCitizenScreen } from '~/modules/RegisterCitizen/Views/RegisterCitizenScreen';
 import { RegisterScreen } from '~/modules/Register/Views/RegisterScreen';
+import { useSelector } from 'react-redux';
+import { AplciationState } from '../@types/Entity/AplicationState';
 
 const Stack = createNativeStackNavigator();
+const StackLogin = createNativeStackNavigator();
 
 export function StackRoutes() {
+  const { isLogged } = useSelector((state: AplciationState) => state.user);
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -19,18 +24,27 @@ export function StackRoutes() {
     >
       <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Login"
-            screenOptions={{ headerShown: false }}
-          >
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen
-              name="RegisterCitizen"
-              component={RegisterCitizenScreen}
-            />
-          </Stack.Navigator>
+          {isLogged ? (
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Register" component={RegisterScreen} />
+
+              <Stack.Screen
+                name="RegisterCitizen"
+                component={RegisterCitizenScreen}
+              />
+            </Stack.Navigator>
+          ) : (
+            <StackLogin.Navigator
+              initialRouteName="Login"
+              screenOptions={{ headerShown: false }}
+            >
+              <StackLogin.Screen name="Login" component={LoginScreen} />
+            </StackLogin.Navigator>
+          )}
         </NavigationContainer>
       </SafeAreaView>
     </KeyboardAvoidingView>
