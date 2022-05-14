4092749e3aecfc825221c481eb9d61a3dba6de87
LinsThi <thiagolins13255@gmail.com>
2022-05-14 15:11:52 -0300

    :sparkles: Created PickerInput component

---
+++ src/modules/RegisterCitizen/components/PickerInput/mock.ts
@@ -0,0 +1,23 @@
+export type VaccinesProps = {
+  id: number;
+  label: string;
+  value: string;
+};
+
+export const vaccinesName = [
+  {
+    id: 0,
+    label: 'Pfizer',
+    value: 'Pfizer',
+  },
+  {
+    id: 1,
+    label: 'Astrazeneca',
+    value: 'Astrazeneca',
+  },
+  {
+    id: 2,
+    label: 'CoronaVac',
+    value: 'Coronavac',
+  },
+];
---
+++ src/modules/RegisterCitizen/components/PickerInput/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+`;
+
+export const ContainerPicker = styled.View`
+  background: #fff;
+  border-radius: 20px;
+  padding-left: 5px;
+`;
+
+export const LabelInput = styled.Text`
+  margin-bottom: 10px;
+  font-size: 17px;
+  font-weight: bold;
+`;
