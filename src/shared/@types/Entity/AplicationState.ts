3dfd089fc4d7ad509a3adbd191146e977b05c6c5
LinsThi <thiagolins13255@gmail.com>
2022-05-15 17:53:27 -0300

    :sparkles: Adding new reducer

--- src/shared/@types/Entity/AplicationState.ts
+++ src/shared/@types/Entity/AplicationState.ts
@@ -1,5 +1,7 @@
+import { CitizensVacinnedState } from '~/shared/store/ducks/citizensVacinned/types';
 import { UserState } from '~/shared/store/ducks/user/types';
 
 export interface AplciationState {
   user: UserState;
+  citizensVacinned: CitizensVacinnedState;
 }
