1fc6275d390903bee5b714eb70e5ddf55505d73e
LinsThi <thiagolins13255@gmail.com>
2022-05-16 15:49:48 -0300

    :sparkles: Implementing themeprovider

--- src/modules/Home/components/FlatList/styles.ts
+++ src/modules/Home/components/FlatList/styles.ts
@@ -12,7 +12,7 @@ export const ContainerItem = styled.View`
   align-items: center;
   justify-content: space-between;
 
-  background: #fff;
+  background: ${({ theme }) => theme.Colors.WHITE};
   margin-top: 30px;
   padding: 10px 20px;
   border-radius: 20px;
@@ -25,20 +25,20 @@ export const ContainerInfoCitizen = styled.View`
 export const ContainerVacinne = styled.View``;
 
 export const NameCitizen = styled.Text`
-  color: #1b2735;
+  color: ${({ theme }) => theme.Colors.NAME_CITIZEN};
   font-weight: bold;
   font-size: 18px;
 `;
 
 export const InformationCitizenCPF = styled.Text`
-  color: #000;
+  color: ${({ theme }) => theme.Colors.BLACK};
   font-size: 13px;
 
   margin-top: 15px;
 `;
 
 export const InformationCitizenBirthday = styled.Text`
-  color: #000;
+  color: ${({ theme }) => theme.Colors.BLACK};
   font-size: 13px;
 `;
 
@@ -48,16 +48,18 @@ export const VacinneImage = styled.Image.attrs({ source: vacinne })`
 `;
 
 export const InformationCitizenDose = styled.Text`
-  color: #000;
+  color: ${({ theme }) => theme.Colors.BLACK};
   font-size: 13px;
 
   margin-top: 5px;
 `;
 
-export const ActivityIndicatorLoading = styled.ActivityIndicator.attrs({
-  size: 'large',
-  color: '#dffe3a',
-})``;
+export const ActivityIndicatorLoading = styled.ActivityIndicator.attrs(
+  ({ theme }) => ({
+    size: 'large',
+    color: theme.Colors.ACTIVITY_INDICATOR,
+  }),
+)``;
 
 export const FlatList = styled.FlatList`
   width: 90%;
